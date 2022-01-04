64f22061c6131f1f27dc62c57d38445413e9f87f
Pavel Sakharov <som3well@gmail.com>
2022-01-05 04:16:15 +0700

    added keyDown listener, now string only can start with minus or number and now you can't type two or more operators in a row

--- calc.js
+++ calc.js
@@ -2,14 +2,30 @@ const   buttons = document.querySelectorAll('[data-button]'),
         submit = document.querySelector('[data-button-calc]')
         inputString = document.querySelector('#inputted')
         clear = document.querySelector('[data-button-clear]')
-        clearAll = document.querySelector('[data-button-clear-all]');
+        clearAll = document.querySelector('[data-button-clear-all]')
+        specialCharacter  = ['*', '/', '+', '-'];
+
 
 const calculateString = () => {
     inputString.innerHTML = new Function('return ' + inputString.innerHTML)();
 }
 
 const addToString = (input) => {
-    if (inputString.innerHTML === '0') inputString.innerHTML = input
+    if (inputString.innerHTML === '0') {
+        switch (input){
+            case '-':
+                inputString.innerHTML = input
+                break
+            case '*':
+            case '/':
+            case '+':
+                inputString.innerHTML = '0'
+                break;
+            default:
+                inputString.innerHTML = input
+        }
+    }
+    else if (specialCharacter.includes(inputString.innerHTML.slice(-1)) && specialCharacter.includes(input)){}
     else inputString.innerHTML += input
 }
 const removeLast = () => {
@@ -27,3 +43,41 @@ buttons.forEach(el => el.addEventListener('click', (event) => {
 submit.addEventListener('click', calculateString)
 clear.addEventListener('click', removeLast)
 clearAll.addEventListener('click', () => {inputString.innerHTML = '0'})
+
+
+const inputKeys = {
+    'Numpad1': '1',
+    'Numpad2': '2',
+    'Numpad3': '3',
+    'Numpad4': '4',
+    'Numpad5': '5',
+    'Numpad6': '6',
+    'Numpad7': '7',
+    'Numpad8': '8',
+    'Numpad9': '9',
+    'NumpadDivide': '/',
+    'NumpadMultiply': '*',
+    'NumpadSubtract': '-',
+    'NumpadAdd': '+',
+    'Digit1': '1',
+    'Digit2': '2',
+    'Digit3': '3',
+    'Digit4': '4',
+    'Digit5': '5',
+    'Digit6': '6',
+    'Digit7': '7',
+    'Digit8': '8',
+    'Digit9': '9',
+    'Backspace': removeLast,
+    'NumpadEnter': calculateString,
+    'Enter': calculateString,
+    'Delete': () => {inputString.innerHTML = '0'}
+};
+
+document.addEventListener('keydown', (event) => {
+    if (inputKeys.hasOwnProperty(event.code)){
+        let code = event.code
+        if (typeof inputKeys[event.code] === "function") inputKeys[event.code]()
+        else addToString(inputKeys[event.code])
+    }
+})
